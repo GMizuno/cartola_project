fa22e23599b09230911bb0a13ae53e07834e5de7
Gabriel Mizuno <gabrielmizuno@gmail.com>
2023-01-29 00:23:34 -0300

    Start refactoring team

--- get_data/models/team.py
+++ get_data/models/team.py
@@ -1,6 +1,15 @@
-from get_data.export import export_team_bronze, export_team_silver
-from cartola_project import config_team_dict
 from decouple import config
+
+from get_data.export import export_team_bronze, export_team_silver
+
+
+def config_team_dict(api_host_key: str = 'API_HOST_KEY',
+                     api_secert_key: str = 'API_SECERT_KEY', ) -> dict:
+    return {
+        'api_host_key': config(api_host_key),
+        'api_secert_key': config(api_secert_key),
+    }
+
 
 params = config_team_dict()
 export_team_bronze(**params)
